dac012fb32f9db369edb3513d86a423a666ed08f
Anil Vettathu <anilv4@users.noreply.github.com>
2024-01-23 20:30:20 +0530

    Update server.go

--- source/server.go
+++ source/server.go
@@ -1,40 +1,53 @@
 package main
 
 import (
-        "context"
-        "log"
-        "net"
-        "os"
+	"context"
+	"log"
+	"net"
+	"os"
 
-        "gogrpcserver/pb"
-        "google.golang.org/grpc"
-        "google.golang.org/grpc/reflection"
+	pb "gogrpcserver/pb" // Make sure this import path matches your setup
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
-        pb.UnimplementedHostnameGetterServer
+	pb.UnimplementedHostnameGetterServer
 }
 
 func (s *server) GetHostname(ctx context.Context, in *pb.HostnameRequest) (*pb.HostnameReply, error) {
-        hostname, err := os.Hostname()
-        if err != nil {
-                return nil, err
-        }
-        return &pb.HostnameReply{Message: "Hostname: " + hostname}, nil
+	// Extract client IP from context
+	p, ok := peer.FromContext(ctx)
+	if ok {
+		clientIP := p.Addr.String()
+		log.Printf("Received GetHostname request from client IP: %s", clientIP)
+	} else {
+		log.Printf("Received GetHostname request but could not determine client IP")
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Error getting hostname: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Sending hostname: %s", hostname)
+	return &pb.HostnameReply{Message: "Hostname: " + hostname}, nil
 }
 
 func main() {
-        lis, err := net.Listen("tcp", ":50051")
-        if err != nil {
-                log.Fatalf("Failed to listen: %v", err)
-        }
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
-        s := grpc.NewServer()
-        pb.RegisterHostnameGetterServer(s, &server{})
-        reflection.Register(s)
+	grpcServer := grpc.NewServer()
+	pb.RegisterHostnameGetterServer(grpcServer, &server{})
+	reflection.Register(grpcServer)
 
-        log.Printf("Server is listening at %v", lis.Addr())
-        if err := s.Serve(lis); err != nil {
-                log.Fatalf("Failed to serve: %v", err)
-        }
+	log.Printf("Server is listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
